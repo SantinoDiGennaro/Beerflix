88bc2e4145b033e683d839f4fd4c67066c6b7b5f
Santino Di Gennaro <santinodg96@gmail.com>
2023-02-19 12:00:20 +0100

    landing page on react

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,25 +1,26 @@
+import { Link } from "react-router-dom"
 import beerflix_logo from "../../assets/Beerflix Logo.png"
 
 export function Navbar(){
     return(
-        <nav class="h-20 w-full px-4 flex justify-between items-center bg-[#ece7d3]">
-                <div class="flex gap-5 items-center">
-                    <img class="w-24 h-12" src={beerflix_logo} alt="Beerflix logo"/>
+        <nav className="h-20 w-full px-4 flex justify-between items-center bg-[#ece7d3]">
+                <div className="flex gap-5 items-center">
+                    <img className="w-24 h-12" src={beerflix_logo} alt="Beerflix logo"/>
 
-                    <div class="dropdown">
-                        <a class="dropdown-toggle text-[#180000] hover:text-[#fb9f1f] no-underline" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <div className="dropdown">
+                        <a className="dropdown-toggle text-[#180000] hover:text-[#fb9f1f] no-underline" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Scopri i prodotti
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="text-[#180000] hover:text-[#fb9f1f] no-underline" href="#">Birre</a></li>
-                            <li><a class="text-[#180000] hover:text-[#fb9f1f] no-underline" href="#">Snack</a></li>
+                            <li><a className="text-[#180000] hover:text-[#fb9f1f] no-underline" href="#">Birre</a></li>
+                            <li><a className="text-[#180000] hover:text-[#fb9f1f] no-underline" href="#">Snack</a></li>
                         </ul>
                     </div>
 
-                    <a href="#" class="text-[#180000] hover:text-[#fb9f1f] no-underline">Come funziona</a>
-                    <a href="#" class="text-[#180000] hover:text-[#fb9f1f] no-underline">Aiuto</a>
+                    <a href="#" className="text-[#180000] hover:text-[#fb9f1f] no-underline">Come funziona</a>
+                    <a href="#" className="text-[#180000] hover:text-[#fb9f1f] no-underline">Aiuto</a>
                 </div>
-                <a href="#" class="flex w-24 h-10 bg-[#fb9f1f] text-[#ece7d3] no-underline justify-center items-center rounded-md border-2 border-[#ece7d3] hover:text-[#fb9f1f] hover:border-[#fb9f1f] hover:bg-[#ece7d3]">Accedi</a>
+                <Link to="login" className="flex w-24 h-10 bg-[#fb9f1f] text-[#ece7d3] no-underline justify-center items-center rounded-md border-2 border-[#ece7d3] hover:text-[#fb9f1f] hover:border-[#fb9f1f] hover:bg-[#ece7d3]">Accedi</Link>
             </nav>
     )
 }
