1c0001970255e02f7a0c74b17355a267e1d8af90
Santino Di Gennaro <santinodg96@gmail.com>
2023-03-24 20:31:54 +0100

    rework navbar

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,7 +1,5 @@
 import { Link } from "react-router-dom";
 import beerflix_logo from "../../assets/Beerflix Logo.png";
-import { useState } from "react";
-import { useEffect } from "react";
 
 export function Navbar() {
     return (
