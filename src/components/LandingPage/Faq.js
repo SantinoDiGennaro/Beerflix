016225772d3fe52b46667500c70c9a620a14d1ce
RaveTheN <peppereres@live.it>
2023-04-26 00:17:07 +0200

    sistemate immagini landing

--- src/components/LandingPage/Faq.js
+++ src/components/LandingPage/Faq.js
@@ -1,182 +1,173 @@
 export function Faq() {
-    return (
-        <div className=" lg:px-80 pb-4">
-            <h1 className="text-2xl font-bold text-center py-4">
-                Altre domande?
-            </h1>
-            <div className="accordion accordion-flush" id="accordionExample">
-                <div className="accordion-item bg-[#ece7d3]">
-                    <h2 className="accordion-header" id="headingOne">
-                        <button
-                            className="accordion-button"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#collapseOne"
-                            aria-expanded="true"
-                            aria-controls="collapseOne"
-                        >
-                            Quali sono le opzioni di abbonamento disponibili?
-                        </button>
-                    </h2>
-                    <div
-                        id="collapseOne"
-                        className="accordion-collapse collapse show"
-                        aria-labelledby="headingOne"
-                        data-bs-parent="#accordionExample"
-                    >
-                        <div className="accordion-body">
-                            <strong>
-                                Offriamo opzioni di abbonamento mensili e
-                                settimanali per box di 3, 4, 5, 6, 7 o 8 birre.
-                            </strong>
-                        </div>
-                    </div>
-                </div>
-                <div className="accordion-item bg-[#ece7d3]">
-                    <h2 className="accordion-header" id="headingTwo">
-                        <button
-                            className="accordion-button collapsed"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#collapseTwo"
-                            aria-expanded="false"
-                            aria-controls="collapseTwo"
-                        >
-                            Posso scegliere le birre da inserire nel mio box?
-                        </button>
-                    </h2>
-                    <div
-                        id="collapseTwo"
-                        className="accordion-collapse collapse"
-                        aria-labelledby="headingTwo"
-                        data-bs-parent="#accordionExample"
-                    >
-                        <div className="accordion-body">
-                            <strong>
-                                Sì, puoi scegliere le bottiglie di birra da
-                                inserire in ogni singolo slot del tuo box.
-                            </strong>
-                        </div>
-                    </div>
-                </div>
-                <div className="accordion-item bg-[#ece7d3]">
-                    <h2 className="accordion-header" id="headingThree">
-                        <button
-                            className="accordion-button collapsed"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#collapseThree"
-                            aria-expanded="false"
-                            aria-controls="collapseThree"
-                        >
-                            Come posso modificare le mie preferenze di birra o
-                            il mio abbonamento?
-                        </button>
-                    </h2>
-                    <div
-                        id="collapseThree"
-                        className="accordion-collapse collapse"
-                        aria-labelledby="headingThree"
-                        data-bs-parent="#accordionExample"
-                    >
-                        <div className="accordion-body">
-                            <strong>
-                                Puoi modificare le tue preferenze di birra e il
-                                tuo abbonamento accedendo al tuo account sul
-                                nostro sito web.
-                            </strong>
-                        </div>
-                    </div>
-                </div>
-                <div className="accordion-item bg-[#ece7d3]">
-                    <h2 className="accordion-header" id="headingFour">
-                        <button
-                            className="accordion-button collapsed"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#collapseFour"
-                            aria-expanded="false"
-                            aria-controls="collapseFour"
-                        >
-                            Quando viene addebitata la mia carta di credito per
-                            l'abbonamento?
-                        </button>
-                    </h2>
-                    <div
-                        id="collapseFour"
-                        className="accordion-collapse collapse"
-                        aria-labelledby="headingFour"
-                        data-bs-parent="#accordionExample"
-                    >
-                        <div className="accordion-body">
-                            <strong>
-                                Addebitiamo la carta di credito degli abbonati
-                                il giorno in cui viene spedito il loro box
-                                mensile o settimanale.
-                            </strong>
-                        </div>
-                    </div>
-                </div>
-                <div className="accordion-item bg-[#ece7d3]">
-                    <h2 className="accordion-header" id="headingFive">
-                        <button
-                            className="accordion-button collapsed"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#collapseFive"
-                            aria-expanded="false"
-                            aria-controls="collapseFive"
-                        >
-                            Posso mettere in pausa o cancellare il mio
-                            abbonamento?
-                        </button>
-                    </h2>
-                    <div
-                        id="collapseFive"
-                        className="accordion-collapse collapse"
-                        aria-labelledby="headingFive"
-                        data-bs-parent="#accordionExample"
-                    >
-                        <div className="accordion-body">
-                            <strong>
-                                Sì, puoi mettere in pausa o cancellare il tuo
-                                abbonamento in qualsiasi momento accedendo al
-                                tuo account sul nostro sito web.
-                            </strong>
-                        </div>
-                    </div>
-                </div>
-                <div className="accordion-item bg-[#ece7d3]">
-                    <h2 className="accordion-header" id="headingSix">
-                        <button
-                            className="accordion-button collapsed"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#collapseSix"
-                            aria-expanded="false"
-                            aria-controls="collapseSix"
-                        >
-                            Quanto tempo ci vuole per ricevere il mio box di
-                            birra?
-                        </button>
-                    </h2>
-                    <div
-                        id="collapseSix"
-                        className="accordion-collapse collapse"
-                        aria-labelledby="headingSix"
-                        data-bs-parent="#accordionExample"
-                    >
-                        <div className="accordion-body">
-                            <strong>
-                                I box vengono spediti entro 3-5 giorni
-                                lavorativi dalla data dell'ordine e dovrebbero
-                                essere consegnati entro 7-10 giorni lavorativi
-                                dalla data di spedizione.
-                            </strong>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className=" lg:px-80 pb-4">
+      <h1 className="text-2xl font-bold text-center py-4">Altre domande?</h1>
+      <div className="accordion accordion-flush" id="accordionExample">
+        <div className="accordion-item bg-[#ece7d3]">
+          <h2 className="accordion-header" id="headingOne">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >
+              Quali sono le opzioni di abbonamento disponibili?
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>
+                Offriamo opzioni di abbonamento mensili e settimanali per box da
+                4, 5, 6, 7 o 8 birre.
+              </strong>
             </div>
+          </div>
         </div>
-    );
+        <div className="accordion-item bg-[#ece7d3]">
+          <h2 className="accordion-header" id="headingTwo">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              Posso scegliere le birre da inserire nel mio box?
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>
+                Sì, puoi scegliere le bottiglie di birra da inserire in ogni
+                singolo slot del tuo box.
+              </strong>
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item bg-[#ece7d3]">
+          <h2 className="accordion-header" id="headingThree">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              Come posso modificare le mie preferenze di birra o il mio
+              abbonamento?
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>
+                Puoi modificare le tue preferenze di birra e il tuo abbonamento
+                accedendo al tuo account sul nostro sito web.
+              </strong>
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item bg-[#ece7d3]">
+          <h2 className="accordion-header" id="headingFour">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseFour"
+              aria-expanded="false"
+              aria-controls="collapseFour"
+            >
+              Quando viene addebitata la mia carta di credito per l'abbonamento?
+            </button>
+          </h2>
+          <div
+            id="collapseFour"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingFour"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>
+                Addebitiamo la carta di credito degli abbonati il giorno in cui
+                viene spedito il loro box mensile o settimanale.
+              </strong>
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item bg-[#ece7d3]">
+          <h2 className="accordion-header" id="headingFive">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseFive"
+              aria-expanded="false"
+              aria-controls="collapseFive"
+            >
+              Posso mettere in pausa o cancellare il mio abbonamento?
+            </button>
+          </h2>
+          <div
+            id="collapseFive"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingFive"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>
+                Sì, puoi mettere in pausa o cancellare il tuo abbonamento in
+                qualsiasi momento accedendo al tuo account sul nostro sito web.
+              </strong>
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item bg-[#ece7d3]">
+          <h2 className="accordion-header" id="headingSix">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseSix"
+              aria-expanded="false"
+              aria-controls="collapseSix"
+            >
+              Quanto tempo ci vuole per ricevere il mio box di birra?
+            </button>
+          </h2>
+          <div
+            id="collapseSix"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingSix"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>
+                I box vengono spediti entro 3-5 giorni lavorativi dalla data
+                dell'ordine e dovrebbero essere consegnati entro 7-10 giorni
+                lavorativi dalla data di spedizione.
+              </strong>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/LandingPage/SocialReview.js
+++ src/components/LandingPage/SocialReview.js
@@ -1,61 +1,61 @@
 import testImg from "../../assets/landing-page-image/rece1.jpg";
-
+import friends_drinking from "../../assets/landing-page-image/friends_drinking.PNG";
+import drinking_together from "../../assets/landing-page-image/drinking_together.PNG";
+import gluten_alcohool from "../../assets/landing-page-image/gluten_alcohool.PNG";
 export function SocialReview() {
-    return (
-        <div className="py-6 lg:px-40 border-b-4 border-[#fb9f1f]">
-            <h1 className="text-lg lg:text-2xl font-bold text-center py-4">
-                Attenzione, il vostro aperitivo non sarà più lo stesso, chiedilo
-                a loro!
-            </h1>
-            <div
-                name="cards-container"
-                className=" flex flex-col justify-around items-center sm:max-lg:items-center gap-3 lg:flex-row lg:items-baseline lg:gap-0"
-            >
-                <div name="card" className="w-[200px]">
-                    <img
-                        src={testImg}
-                        className="w-[200px] h-[200px]"
-                        alt="test"
-                    />
-                    <h4 className="py-2 text-center">
-                        "Sperimentare gusti nuovi e divertirci con gli amici
-                        diventa ancora più facile grazie a @beerflixitalia 💚"{" "}
-                    </h4>
-                </div>
-                <div name="card" className="w-[200px]">
-                    <img
-                        src={testImg}
-                        className="w-[200px] h-[200px]"
-                        alt="test"
-                    />
-                    <h4 className="py-2 text-center">
-                        "Durante l’inverno restare a casa può diventare una
-                        necessità, ma ero stanco delle solite birre del
-                        supermercato. Finalmente ho scoperto le box di
-                        @beerflixitalia"{" "}
-                    </h4>
-                </div>
-                <div name="card" className="w-[200px]">
-                    <img
-                        src={testImg}
-                        className="w-[200px] h-[200px]"
-                        alt="test"
-                    />
-                    <h4 className="py-2 text-center">
-                        "La box è riciclabile, e la maggior parte dei marchi
-                        sono di produzione italiana. L’impatto ambientale è pari
-                        allo 0"{" "}
-                    </h4>
-                </div>
-                <div name="card" className="w-[200px]">
-                    <img
-                        src={testImg}
-                        className="w-[200px] h-[200px]"
-                        alt="test"
-                    />
-                    <h4 className="py-2 text-center"></h4>
-                </div>
-            </div>
+  return (
+    <div className="py-6 lg:px-40 border-b-4 border-[#fb9f1f]">
+      <h1 className="text-lg lg:text-2xl font-bold text-center py-4">
+        Attenzione, il vostro aperitivo non sarà più lo stesso, chiedilo a loro!
+      </h1>
+      <div
+        name="cards-container"
+        className=" flex flex-col justify-around items-center sm:max-lg:items-center gap-3 lg:flex-row lg:items-baseline lg:gap-0"
+      >
+        <div name="card" className="w-[200px]">
+          <img
+            src={drinking_together}
+            className="w-[200px] h-[200px]"
+            alt="test"
+          />
+          <h4 className="py-2 text-center">
+            "Sperimentare gusti nuovi e divertirci con gli amici diventa ancora
+            più facile grazie a @beerflixitalia 💛"{" "}
+          </h4>
         </div>
-    );
+        <div name="card" className="w-[200px]">
+          <img src={testImg} className="w-[200px] h-[200px]" alt="test" />
+          <h4 className="py-2 text-center">
+            "Durante l’inverno restare a casa può diventare una necessità, e
+            senza il nostro pub preferito dovevamo accontentarci di quello che
+            offriva il supermercato. Finalmente ho scoperto le box di
+            @beerflixitalia 🍻"{" "}
+          </h4>
+        </div>
+        <div name="card" className="w-[200px]">
+          <img
+            src={friends_drinking}
+            className="w-[200px] h-[200px]"
+            alt="test"
+          />
+          <h4 className="py-2 text-center">
+            "La box è riciclabile, e la maggior parte dei marchi sono di
+            produzione italiana. L’impatto ambientale è pari a 0 🌱"{" "}
+          </h4>
+        </div>
+        <div name="card" className="w-[200px]">
+          <img
+            src={gluten_alcohool}
+            className="w-[200px] h-[200px]"
+            alt="test"
+          />
+          <h4 className="py-2 text-center">
+            "Mia marito è un grande amante della birra, ma io sono astemia e mia
+            figlia è celiaca. Adesso non vediamo l'ora che arrivi la box per
+            provare gusti nuovi e poter festeggiare tutti insieme. "
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
 }
